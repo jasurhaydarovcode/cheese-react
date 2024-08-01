cb2de9f5e2b2cf8399b37e11e4768e5e43d5809a
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-08-02 00:19:43 +0500

    NotFound page Updated

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -3,27 +3,34 @@ import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
+  const bgImage = {
+    backgroundImage: "url('https://cdn.dribbble.com/users/589596/screenshots/11205169/day_8_-_404_page.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "100vh",
+    color: "white"
+  }
   return (
     <>
       <Helmet>
         <title>404 Page Not Found</title>
       </Helmet>
-      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div style={bgImage} className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
         <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
           <div className="relative">
-            <div className="absolute">
+            <div className="absolte">
               <div>
-                <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                <h1 className="my-2 bg-gray-400 font-bold text-6xl">
                   Похоже, ты нашел дверь в великое ничто.
                 </h1>
-                <p className="my-2 text-gray-800">Извини за это! Пожалуйста, посетите нашу домашнюю страницу, чтобы добраться туда, куда вам нужно.</p>
+                <p className="my-2 bg-gray-400 text-2xl">Извини за это! Пожалуйста, посетите нашу домашнюю страницу, чтобы добраться туда, куда вам нужно.</p>
                 <Link to="/">
                   <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text  -center bg-sariq text-white hover:bg-sariq-100 transition focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Отведи меня домой!</button>
                 </Link>
               </div>
             </div>
             <div>
-              <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+              {/* <img src="https://i.ibb.co/G9DC8S0/404-2.png" /> */}
             </div>
           </div>
         </div>
